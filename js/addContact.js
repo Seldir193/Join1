899d162cd3155e0349ae4a2af6ee4509f3c3270e
Besim91 <78695114+Besim91@users.noreply.github.com>
2024-02-29 22:26:44 +0100

    Add input, img & buttons to lowerBody slideCard

--- js/addContact.js
+++ js/addContact.js
@@ -1,7 +1,9 @@
 function addContact() {
   document.getElementById("blurContainer").classList.remove("d-none");
+  document.getElementById("addContactSlideCard").classList.add("slideOpen");
 }
 
 function closeAddContact() {
   document.getElementById("blurContainer").classList.add("d-none");
+  document.getElementById("addContactSlideCard").classList.remove("slideOpen");
 }
